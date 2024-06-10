c946873d87987f98c4390b4808bc9bb06b3702af
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-10 13:11:52 +0200

    More props

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable react/prop-types */
 const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Mikhail'
+  const age = 28
   return (
     <div>
       <h1>Greetings</h1>
 
-      <Hello name="Mikhail"/>
+      <Hello name={name} age={age}/>
     </div>
   )
 }
