d0c2fb972c5f10d4ae4e8bb03914d9dfce9ab244
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-13 11:01:41 +0200

    Ex 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,52 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
 
+const Button = ({ text, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Feedback = ({stats}) => {
+  const { good, neutral, bad, setGood, setNeutral, setBad } = stats
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button text="good" onClick={handleGood}/>
+      <Button text="neutral" onClick={handleNeutral}/>
+      <Button text="bad" onClick={handleBad}/>
+    </div>
+  )
+}
+
+const Statistics = ({stats}) => {
+  const { good, neutral, bad } = stats
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p> Good {good} </p>
+      <p> Neutral {neutral} </p>
+      <p> Bad {bad} </p>
+    </div>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const stats = {good, neutral, bad, setGood, setNeutral, setBad }
+  
   return (
     <div>
-      code here
+      <Feedback stats={stats}/>
+      <Statistics stats={stats}/>
     </div>
   )
 }
