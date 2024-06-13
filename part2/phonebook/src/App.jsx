884bb143c70f94d547e2a2ace2313d0e2a3b22c0
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-13 15:57:24 +0200

    Ex 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -13,7 +13,13 @@ const App = () => {
     const personObject = {
       name: newName,
     };
-    setPersons(persons.concat(personObject));
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    }
+    else{
+      setPersons(persons.concat(personObject));
+      setNewName("");
+    }
   };
 
   return (
