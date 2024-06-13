59fae0f1d6254dc404e766d455d20b0ee294a024
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-13 11:05:12 +0200

    Ex 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -29,6 +29,14 @@ const Statistics = ({stats}) => {
   const total = good + neutral + bad
   const average = (good - bad) / total
   const positive = (good / total) * 100
+  if (total === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>Statistics</h1>
