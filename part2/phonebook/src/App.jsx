efacd4c52021f124201a6a8ed3be950c89582ae8
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-13 16:12:42 +0200

    Ex 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const changeNameForm = (event) => {
     setNewName(event.target.value);
@@ -13,37 +19,51 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const changeSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1, // Ensure unique id
     };
     if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
       setPersons(persons.concat(personObject));
       setNewName("");
+      setNewNumber(""); // Reset the newNumber field as well
     }
   };
 
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Search</h2>
+      <div>
+        <input value={searchTerm} onChange={changeSearchTerm} />
+      </div>
+      <h2>Add a number</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input onChange={changeNameForm} />
+          name: <input value={newName} onChange={changeNameForm} />
         </div>
         <div>
-          number: <input onChange={changeNumberForm} />
+          number: <input value={newNumber} onChange={changeNumberForm} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
+      {filteredPersons.map((person) => (
+        <div key={person.id}>
           {person.name} {person.number}
         </div>
       ))}
