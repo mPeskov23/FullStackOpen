a5eb20e9d18792a16bdfc5b1eaed3958ead29a13
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-14 10:55:46 +0200

    Ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import FilterForm from "./components/FilterForm";
 import SubmitForm from "./components/SubmitForm";
 import Numbers from "./components/Numbers";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+
   const [newName, setNewName] = useState("");
 
   const [newNumber, setNewNumber] = useState("");
 
   const [searchTerm, setSearchTerm] = useState("");
 
-  const submitInfo = [newName, setNewName, newNumber, setNewNumber, persons, setPersons]
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  const submitInfo = [
+    newName,
+    setNewName,
+    newNumber,
+    setNewNumber,
+    persons,
+    setPersons,
+  ];
 
   return (
     <div>
@@ -25,7 +35,7 @@ const App = () => {
       <h2>Add a number</h2>
       <SubmitForm submitInfo={submitInfo} />
       <h2>Numbers</h2>
-      <Numbers persons={persons} searchTerm={searchTerm}/>
+      <Numbers persons={persons} searchTerm={searchTerm} />
     </div>
   );
 };
