5f3829acc90e01dd16e90a1c3da9a1fbe1a753d4
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-16 17:53:10 +0200

    Ex 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const phonebook = [
+let phonebook = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -44,6 +44,12 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  phonebook = phonebook.filter((person) => person.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
