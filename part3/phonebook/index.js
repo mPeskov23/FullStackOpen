139c759253e90487eeb08a894aeea7657448af99
mPeskov23 <sorrowdelivery@gmail.com>
2024-06-16 18:02:17 +0200

    Ex 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -24,6 +24,7 @@ let phonebook = [
 ];
 
 const app = express();
+app.use(express.json());
 
 app.get("/api/persons", (req, res) => {
   res.json(phonebook);
@@ -50,6 +51,31 @@ app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   phonebook = phonebook.filter((person) => person.id !== id);
   res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  if (phonebook.find((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 10000),
+    name: body.name,
+    number: body.number,
+  };
+
+  phonebook = phonebook.concat(person);
+  res.json(person);
 });
 
 const PORT = 3001;
